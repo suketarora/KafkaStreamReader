856610e64cd04b0ec62262448622683e1003b096
suketarora <suket.arora@islabs.io>
2019-01-24 10:37:07 +0530

    Added Notebooks,resources and Readme files.

--- build.sbt
+++ build.sbt
@@ -19,7 +19,8 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
     "org.apache.spark" %% "spark-sql" % sparkVersion,
     "org.apache.spark" %% "spark-streaming" % sparkVersion,
-  "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4.0"
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion,
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion
   
 
  // the rest of the file
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,49 +1,33 @@
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.clients.consumer._
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark._
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._ 
-
-
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.sql.SparkSession
 
 object Main extends App {
-
-
-  
-    override def main(arg: Array[String]): Unit = {
-  
-      
-       val conf = new SparkConf().setAppName("KafkaStreaming").setMaster("local[*]").set( "spark.driver.memory", "4g" )  
-       val ssc = new StreamingContext(conf, Seconds(10))
-       
-       val kafkaParams = Map[String, Object](
-                    "bootstrap.servers" -> "localhost:9092",  //localhost:9092,kafka-broker1:9092
-                    "key.deserializer" -> classOf[StringDeserializer],
-                    "value.deserializer" -> classOf[StringDeserializer],
-                    "group.id" -> "group one",   //use_a_separate_group_id_for_each_stream
-                    "auto.offset.reset" -> "latest",
-                    "enable.auto.commit" -> (false: java.lang.Boolean)
-                  )
-
-        val topics = Array("test")
+   override def main(arg: Array[String]): Unit = {
+     val spark =SparkSession.builder().appName("FlightsProducer").master("local[2]").getOrCreate()
+  val sc =spark.sparkContext
 
 
 
-        val stream = KafkaUtils.createDirectStream[String, String](
-          ssc,
-          PreferConsistent,
-          Subscribe[String, String](topics, kafkaParams)
-        )
-          
-        val streamTuple = stream.map(record => (record.key, record.value))
-        streamTuple.print()
-        ssc.start()
-        ssc.awaitTermination()
-    }
+  val df = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "test")
+    .option("startingOffsets", "earliest")
+    .load()
 
+  val query = df.writeStream
+    .outputMode("update")
+    .format("console")
+    .trigger(Trigger.ProcessingTime(10000))
+//          .option("batch.size",10000)
+    .start()
+
+
+  query.awaitTermination();
+   }
 }
-
